662c88ea4a54982d32538ba9f88017713736d932
Wim Stalmans <wim.stalmans@gmail.com>
2024-04-20 08:54:06 +0200

    rationalisation of index code

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 require('dotenv').config();
 const ethers = require('ethers');
 
-const network = {
-    name: 'sepolia',
-    chainId: 11155111 // This is the chain ID for Sepolia
-};
-const provider = new ethers.providers.InfuraProvider('sepolia', process.env.INFURA_API_KEY);
-
 async function main() {
-    const counterContract = new ethers.Contract(
+    const Contract = new ethers.Contract(
         '0xF98569F595Df44242c34329c6F4d17221c71107b',
         '[{"inputs":[],"name":"count","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decr","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
-        provider
+        new ethers.providers.InfuraProvider('sepolia', process.env.INFURA_API_KEY)
     );
 
-    const currentCounterValue = await counterContract.get();
+    const currentCounterValue = await Contract.get();
     console.log(currentCounterValue.toString());
 }
 
